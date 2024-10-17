691c5755e77f043e87d833ded0506b0f993aaea9
corradodebari <corrado.de.bari@oracle.com>
2024-10-17 18:57:40 +0200

    base Spring AI template to run configuration tested

---
+++ spring_ai/README.md
@@ -0,0 +1,71 @@
+# Spring AI with OpenAI
+
+## How to run:
+Create a `start.sh` script putting:
+```
+export SPRING_AI_OPENAI_API_KEY=$OPENAI_API_KEY
+export DB_DSN="jdbc:oracle:thin:@localhost:1521/FREEPDB1"
+export DB_USERNAME=<DB_USER_NAME>
+export DB_PASSWORD=<DB_USER_PASSWORD>
+export DISTANCE_TYPE=COSINE
+export OPENAI_CHAT_MODEL=gpt-4o-mini
+export OPENAI_EMBEDDING_MODEL=text-embedding-3-small
+export OLLAMA_CHAT_MODEL="llama3.1"
+export OLLAMA_EMBEDDING_MODEL=mxbai-embed-large
+export OLLAMA_BASE_URL="http://<OLLAMA_SERVER>:11434"
+export CONTEXT_INSTR="You are an assistant for question-answering tasks. Use the retrieved Documents and history to answer the question as accurately and comprehensively as possible. Keep your answer grounded in the facts of the Documents, be concise, and reference the Documents where possible. If you don't know the answer, just say that you are sorry as you don't haven't enough information."
+export TOP_K=5
+export VECTOR_STORE=$2
+mvn spring-boot:run -P "$1"
+```
+
+
+### ollama start example:
+```
+./start.sh ollama MXBAI_EMBED_LARGE_512_103_COSINE
+```
+
+### full openai example: 
+```
+./start.sh openai TEXT_EMBEDDING_3_SMALL_8191_1639_COSINE
+```
+This project contains a web service that will accept HTTP GET requests at
+
+* `http://localhost:8080/ai/`
+* `http://localhost:8080/rag/`
+* `http://localhost:8080/search/`
+
+
+There is optional `message` parameter whose default value is "Tell me a joke".
+
+The response to the request is from the OpenAI ChatGPT Service.
+
+Call example: 
+
+```
+curl --get --data-urlencode 'message=Can I use any kind of development environment to run the example?' localhost:8080/rag| jq .
+```
+the response without RAG:
+
+```
+{
+  "completion": ""Yes, you can use any kind of development environment to run the example, but the guide specifically mentions using IntelliJ IDEA as the integrated development environment (IDE) for ease in developing the application. It is recommended to use an IDE to create and update the files for the application, but it does not restrict you to only using IntelliJ; other environments can also be utilized as long as they support Java development (Document 4.1.5)."."
+}
+```
+
+## Prerequisites
+
+Before using the AI commands, make sure you have a developer token from OpenAI.
+
+Create an account at [OpenAI Signup](https://platform.openai.com/signup) and generate the token at [API Keys](https://platform.openai.com/account/api-keys).
+
+The Spring AI project defines a configuration property named `spring.ai.openai.api-key` that you should set to the value of the `API Key` obtained from `openai.com`.
+
+Exporting an environment variable is one way to set that configuration property.
+```shell
+export SPRING_AI_OPENAI_API_KEY=<INSERT KEY HERE>
+```
+
+Setting the API key is all you need to run the application.
+However, you can find more information on setting started in the [Spring AI reference documentation section on OpenAI Chat](https://docs.spring.io/spring-ai/reference/api/clients/openai-chat.html).
+
---
+++ spring_ai/src/main/java/org/springframework/ai/openai/samples/helloworld/AIController.java
@@ -0,0 +1,175 @@
+package org.springframework.ai.openai.samples.helloworld;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.ExtractedTextFormatter;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.SimpleVectorStore.Similarity;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ai.vectorstore.OracleVectorStore;
+
+
+import jakarta.annotation.PostConstruct;
+
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+import java.util.Iterator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+class AIController {
+
+	@Autowired
+	private final OracleVectorStore vectorStore;
+
+	@Autowired
+	private final EmbeddingModel embeddingModel; 
+	
+	@Autowired
+	private final ChatClient chatClient;
+
+	@Value("${aims.vectortable.name}")
+    private String legacyTable;
+
+	@Value("${aims.context_instr}")
+    private String contextInstr;
+
+	@Value("${aims.rag_params.search_type}")
+	private String searchType;
+
+	@Value("${aims.rag_params.top_k}")
+	private int TOPK;
+
+	@Autowired
+    private JdbcTemplate jdbcTemplate;
+
+	private static final Logger logger = LoggerFactory.getLogger(AIController.class);
+	
+	AIController(ChatClient chatClient, EmbeddingModel embeddingModel, OracleVectorStore vectorStore) {
+
+		this.chatClient = chatClient;
+		this.embeddingModel = embeddingModel;
+		this.vectorStore = vectorStore;
+
+	}
+
+	@GetMapping("/ai")
+	Map<String, String> completion(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+
+			return Map.of(
+					"completion",
+					chatClient.prompt()
+							.user(message)
+							.call()
+							.content());
+	}
+
+	@PostConstruct
+	public void insertData() {	
+			String sql = "INSERT INTO SPRING_AI_VECTORS (ID, CONTENT, METADATA, EMBEDDING) " +
+						 "SELECT ID, TEXT, METADATA, EMBEDDING FROM "+ legacyTable;
+			// Execute the insert
+			jdbcTemplate.update(sql);
+	}
+ 
+	public Prompt promptEngineering(String message, String contextInstr ) {
+
+		String template = """
+				DOCUMENTS:
+				{documents}
+
+				QUESTION:
+				{question}
+
+				INSTRUCTIONS:""";
+
+		String default_Instr ="""
+		 		Answer the users question using the DOCUMENTS text above.
+				Keep your answer ground in the facts of the DOCUMENTS.
+				If the DOCUMENTS doesn’t contain the facts to answer the QUESTION, return:
+				I'm sorry but I haven't enough information to answer.
+				""";
+
+		template = template + "\n" + contextInstr;
+
+		List<Document> similarDocuments = this.vectorStore.similaritySearch(
+				SearchRequest.query(message).withTopK(TOPK));
+
+		StringBuilder context = createContext(similarDocuments);
+
+		PromptTemplate promptTemplate = new PromptTemplate(template);
+
+		Prompt prompt = promptTemplate.create(Map.of("documents", context, "question", message));
+
+		logger.info(prompt.toString());
+
+		return prompt;
+
+	}
+
+	StringBuilder createContext(List<Document> similarDocuments) {
+		String START = "\n<article>\n";
+		String STOP = "\n</article>\n";
+
+		Iterator<Document> iterator = similarDocuments.iterator();
+		StringBuilder context = new StringBuilder();
+		while (iterator.hasNext()) {
+			Document document = iterator.next();
+			context.append(document.getId() + ".");
+			context.append(START + document.getFormattedContent() + STOP);
+		}
+		return context;
+	}
+
+	@GetMapping("/rag")
+	Map<String, String> completionRag(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+
+		Prompt prompt = promptEngineering(message,contextInstr);
+		logger.info(prompt.getContents());
+
+		return Map.of(
+				"completion",
+				chatClient.prompt(prompt)
+						.call()
+						.content());
+	}
+
+	@GetMapping("/search")
+	List<Map<String, Object>> search(@RequestParam(value = "message", defaultValue = "Tell me a joke") String query, @RequestParam(value = "topk", defaultValue = 5 ) Integer topK) {
+		
+		List<Document> similarDocs = vectorStore.similaritySearch(SearchRequest.defaults()
+		.withQuery(query)
+		.withTopK(topK));
+
+		List<Map<String, Object>> resultList = new ArrayList<>();
+		for (Document d : similarDocs) {
+            Map<String, Object> metadata = d.getMetadata();
+            Map doc = new HashMap<>();
+            doc.put("id", d.getId());
+            resultList.add(doc);
+        };
+        return resultList;
+	}
+}
